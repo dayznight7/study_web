7aa94ebc06c6bfcff469c1a0619686238c955e1a
dayznight7 <dayznight7@gmail.com>
2022-02-10 00:01:44 +0900

    Do it ~p605

--- js/browser.js
+++ js/browser.js
@@ -1,7 +1,7 @@
-window.open("index.html", "pop", "width=500, height=400, left=100, top=200");
+window.open("browser2.html", "pop", "width=500, height=400, left=100, top=200");
 
 function openPopup() {
-  var newWin = window.open("index.html", "pop", "width=500, height=400");
+  var newWin = window.open("browser2.html", "pop", "width=500, height=400");
   if (newWin == null) {
     alert("팝업이 차단되어 있습니다 팝업 차단을 해제해 주세요");
   }
@@ -9,3 +9,8 @@ function openPopup() {
 }
 
 openPopup();
+
+document.write("screen.availWidth : " + screen.availWidth + "<br>");
+document.write("screen.availHeight : " + screen.availHeight + "<br>");
+document.write("screen.width : " + screen.width + "<br>");
+document.write("screen.height : " + screen.height + "<br>");
